c1c9c52b63491ce0e8b7b39b28958d49b6dcc33e
Sri <ninjawinner00@gmail.com>
2024-10-07 17:03:27 +0545

    Add solution for climbing stairs problem using dynamic programming

---
+++ 70.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        @cache
+        def get_step(n):
+            if n < 0:
+                return 0
+            if n in [1, 2]:
+                return n
+            return get_step(n - 1) + get_step(n - 2)
+
+        return get_step(n)
